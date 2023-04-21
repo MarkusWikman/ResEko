d61054c3214e526c9d24b36fedb6c65e8a3d1622
Markus Wikman <Wikman.markus@gmail.com>
2023-04-21 14:22:51 +0200

    Fixade radioknappar så att man kan välja antingen restaurang eller redovisning i kontaktformuläret

--- ResEko/Migrations/20230421115818_Second migration.Designer.cs
+++ ResEko/Migrations/20230421115818_Second migration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResEko.Models;
 
@@ -10,9 +11,11 @@ using ResEko.Models;
 namespace ResEko.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230421115818_Second migration")]
+    partial class Secondmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ResEko/Models/Customer.cs
+++ ResEko/Models/Customer.cs
@@ -1,5 +1,13 @@
-﻿namespace ResEko.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ResEko.Models
 {
+    public enum ResOrRed
+    {
+        Restaurang, 
+        Redovisning
+    }
     public class Customer
     {
         public int Id { get; set; }
@@ -7,5 +15,8 @@
         public string Email { get; set; }
         public string Phonenumber { get; set; }
         public string ? Comment { get; set; }
+        [Display(Name = "")]
+        public ResOrRed ResOrRed { get; set; }
+
     }
 }
